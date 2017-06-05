73b768c6266922334b9cabfdddcfd9df49f548ed
kenshimokak <rezaei6507@gmail.com>
2017-06-06 03:28:46 +0430

    Add Bullet list, Numbered list, Special character and ... to TinyMCE editor

--- publishable/assets/js/voyager_tinymce.js
+++ publishable/assets/js/voyager_tinymce.js
@@ -1,32 +1,36 @@
 function setImageValue(url){
-  $('.mce-btn.mce-open').parent().find('.mce-textbox').val(url);
+    $('.mce-btn.mce-open').parent().find('.mce-textbox').val(url);
 }
 
 $(document).ready(function(){
 
-  $.ajaxSetup({
-      headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-  });
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
-  tinymce.init({
-    menubar: false,
-    selector:'textarea.richTextBox',
-    skin: 'voyager',
-    min_height: 600,
-    resize: 'vertical',
-    plugins: 'link, image, code, youtube, giphy, table, textcolor',
-    extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-    file_browser_callback: function(field_name, url, type, win) {
+    tinymce.init({
+        menubar: false,
+        selector:'textarea.richTextBox',
+        skin: 'voyager',
+        min_height: 600,
+        resize: 'vertical',
+        extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
+        file_browser_callback: function(field_name, url, type, win) {
             if(type =='image'){
-              $('#upload_file').trigger('click');
+                $('#upload_file').trigger('click');
             }
         },
-    toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table youtube giphy | code',
-    convert_urls: false,
-    image_caption: true,
-    image_title: true
-  });
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste code'
+        ],
+        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code',
+        convert_urls: false,
+        image_caption: true,
+        image_title: true
+    });
 
 });
